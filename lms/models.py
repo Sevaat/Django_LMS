a5e2d4a370c16ee38a954d94318aedfd7cce19fd
Tkachenko Vsevolod <sevaatmail@gmail.com>
2026-01-19 23:39:37 +0500

    Добавлены модели Курс и Урок
    Модель курс содержит название, превью, описание. Модель урок содержит название, описание, превью, ссылку на видеоматериал. Между уроком и курсом реализована связь. Модели находятся в приложении lms.

--- lms/models.py
+++ lms/models.py
@@ -1,3 +1,33 @@
+from typing import Any
+
 from django.db import models
 
-# Create your models here.
+
+class Course(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Название", help_text="Укажите название курса")
+    description = models.CharField(max_length=500, verbose_name="Описание", help_text="Укажите описание курса")
+    image = models.ImageField(upload_to="course/preview", blank=True, null=True, verbose_name="Превью", help_text="Добавьте превью курса")
+
+    class Meta:
+        verbose_name = "Курс"
+        verbose_name_plural = "Курсы"
+        ordering = ["name"]
+
+    def __str__(self) -> Any:
+        return self.email
+
+
+class Lesson(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Название", help_text="Укажите название урока")
+    description = models.CharField(max_length=500, verbose_name="Описание", help_text="Укажите описание урока")
+    image = models.ImageField(upload_to="lesson/preview", blank=True, null=True, verbose_name="Превью", help_text="Добавьте превью урока")
+    video_link = models.CharField(max_length=200, verbose_name="Ссылка на видео", help_text="Укажите ссылку на видеоматериал")
+    course = models.ForeignKey(Course, on_delete=models.SET_NULL, blank=True, null=True, verbose_name="Категория продукта", help_text="Укажите принадлежность к курсу")
+
+    class Meta:
+        verbose_name = "Урок"
+        verbose_name_plural = "Уроки"
+        ordering = ["name", "course"]
+
+    def __str__(self) -> Any:
+        return self.name
