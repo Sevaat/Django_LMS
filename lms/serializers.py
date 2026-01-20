f402da8066f7c765b8fbc2401d883b209ef7ade4
Tkachenko Vsevolod <sevaatmail@gmail.com>
2026-01-20 22:08:21 +0500

    Подсчет уроков в курсе
    Для модели курса добавлен в сериализатор поле вывода количества уроков. Поле реализовано с помощью SerializerMethodField.

--- lms/serializers.py
+++ lms/serializers.py
@@ -1,4 +1,4 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
 
 from lms.models import Course, Lesson
 
@@ -7,6 +7,17 @@ class CourseSerializer(ModelSerializer):
     class Meta:
         model = Course
         fields = "__all__"
+
+
+class CourseDetailSerializer(ModelSerializer):
+    count_lessons = SerializerMethodField()
+
+    def get_count_lessons(self, obj):
+        return Lesson.objects.filter(course=obj).count()
+
+    class Meta:
+        model = Course
+        fields = ("name", "description", "image", "count_lessons")
 
 
 class LessonSerializer(ModelSerializer):
--- lms/urls.py
+++ lms/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
     path("lesson/", LessonListAPIView.as_view(), name="lesson_list"),
     path("lesson/create/", LessonCreateAPIView.as_view(), name="lesson_create"),
     path("lesson/<int:pk>/", LessonRetrieveAPIView.as_view(), name="lesson_retrieve"),
-    path("lesson/<int:pk>/update", LessonUpdateAPIView.as_view(), name="lesson_update"),
+    path("lesson/<int:pk>/update/", LessonUpdateAPIView.as_view(), name="lesson_update"),
     path("lesson/<int:pk>/destroy/", LessonDestroyAPIView.as_view(), name="lesson_destroy"),
 ]
 
