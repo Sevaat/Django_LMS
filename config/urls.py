8dcef188589dd3a4c5fe5c77ad43878d70aa3b9e
Tkachenko Vsevolod <sevaatmail@gmail.com>
2026-04-02 00:33:26 +0500

    Повторная проба автоматической насйтроки после deploy

--- config/urls.py
+++ config/urls.py
@@ -3,8 +3,11 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+from .views import home, health
 
 urlpatterns = [
+    path("", home, name="home"),
+    path("health/", health, name="health"),
     path("admin/", admin.site.urls),
     path("course/", include("lms.urls", namespace="course")),
     path("users/", include("users.urls", namespace="users")),
